3ac84f01567317a99d918c4ddea075bd1488eefa
LightAlykard <lightalykard@yandex.ru>
2019-12-23 16:44:22 +0300

    Update ocheredi.go

--- ocheredi.go
+++ ocheredi.go
@@ -5,23 +5,11 @@ var x []string
 func Push(str string) {
 	x = append(x, str)
 }
-func UnPush() string {
-	var sub []string
-
-	elnum := len(x)
-
-	if elnum == 0 {
-		return ""
-	}
-	first := x[0]
-	if elnum == 1 {
-
-		x[0] = ""
-		return first
-	}
-	for i := 1; i < elnum; i++ {
-		sub = append(sub, x[i])
-	}
-	x = sub
-	return first
+func Pop() string {
+    if len(x) == 0 {
+        return ""
+    }
+    elem := x[0]
+    x = x[1:]
+    return elem
 }
